a75dd003eb816032395306ea1565d724799f124f
NotHydra <rizky.irswanda115@gmail.com>
2024-11-25 23:46:31 +0800

    feat: search engine priorty, exact key -> exact description -> included key -> included description

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,63 @@
 <script setup lang="ts">
-import { computed, ref, watch } from "vue"
+import { computed, ref, watch } from "vue";
 
-import { useDebouncedRef } from "@/composables"
-import { Node, RedBlackTree } from "@/structures/red-black-tree"
-import { EN_RBT, ID_RBT } from "@/helpers/dictionary"
-import { debounce } from "@/helpers/debounce"
-import { Language } from "@/types/dictionary"
+import { useDebouncedRef } from "@/composables";
+import { Node, RedBlackTree } from "@/structures/red-black-tree";
+import { EN_RBT, ID_RBT } from "@/helpers/dictionary";
+import { debounce } from "@/helpers/debounce";
+import { Language } from "@/types/dictionary";
 
-import Button from "primevue/button"
-import InputText from "primevue/inputtext"
-import SelectButton from "primevue/selectbutton"
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import SelectButton from "primevue/selectbutton";
 
-const languageOptions: { label: string, value: Language }[] = [
-  { label: "Indonesian", value: Language.BAHASA_INDONESIA },
-  { label: "English", value: Language.ENGLISH },
-]
+const languageOptions: { label: string; value: Language }[] = [
+    { label: "Indonesian", value: Language.BAHASA_INDONESIA },
+    { label: "English", value: Language.ENGLISH },
+];
 
 const selectedLanguage = ref<Language>(Language.BAHASA_INDONESIA);
 const searchQuery = useDebouncedRef<string>("", 200);
 
-const activeRBT = computed<RedBlackTree>(() => selectedLanguage.value === Language.BAHASA_INDONESIA ? ID_RBT : EN_RBT)
+const activeRBT = computed<RedBlackTree>(() => (selectedLanguage.value === Language.BAHASA_INDONESIA ? ID_RBT : EN_RBT));
 const result = computed<Node[]>(() => activeRBT.value.getNodesBySimiliarity(searchQuery.value));
 
 const lastResultGimmick = computed<(() => void) | null>(() => result.value[0]?.gimmick);
 watch(lastResultGimmick, (gimmick: (() => void) | null) => {
-  if (!gimmick) {
-    return;
-  }
+    if (!gimmick) {
+        return;
+    }
 
-  const debouncedGimmick = debounce(gimmick, 1000);
-  debouncedGimmick();
-})
+    const debouncedGimmick = debounce(gimmick, 1000);
+    debouncedGimmick();
+});
 </script>
 
 <template>
-  <nav class="container mx-auto px-4 py-2">
-    <div class="w-100 text-end">
-      <Button icon="iconify fluent--weather-moon-24-filled" variant="text" rounded></Button>
-    </div>
-  </nav>
-
-  <main class="container mx-auto pt-24 pb-12 px-4">
-    <section class="text-center">
-      <h1 class="text-4xl">Lexionary</h1>
-      <!-- Ganti taglinenya nanti -->
-      <p class="mt-3">Find your lexical needs (tentative).</p>
-
-      <div class="mt-8">
-        <div class="flex gap-x-4 justify-center">
-          <SelectButton v-model="selectedLanguage" :options="languageOptions" optionValue="value" optionLabel="label"></SelectButton>
+    <nav class="container mx-auto px-4 py-2">
+        <div class="w-100 text-end">
+            <Button icon="iconify fluent--weather-moon-24-filled" variant="text" rounded></Button>
         </div>
-        <InputText v-model="searchQuery" placeholder="Search your word" class="mt-4"></InputText>
-      </div>
-    </section>
+    </nav>
 
-    <section v-if="searchQuery">
-      <p v-for="item in result" :key="item.key">{{ item.toString() }}</p>
-    </section>
-  </main>
+    <main class="container mx-auto pt-24 pb-12 px-4">
+        <section class="text-center">
+            <h1 class="text-4xl">Lexionary</h1>
+            <!-- Ganti taglinenya nanti -->
+            <p class="mt-3">Find your lexical needs (tentative).</p>
+
+            <div class="mt-8">
+                <div class="flex gap-x-4 justify-center">
+                    <SelectButton v-model="selectedLanguage" :options="languageOptions" optionValue="value" optionLabel="label"></SelectButton>
+                </div>
+                <InputText v-model="searchQuery" placeholder="Search your word" class="mt-4"></InputText>
+            </div>
+        </section>
+
+        <section v-if="searchQuery">
+            <p v-for="item in result" :key="item.key">{{ item.toString() }}</p>
+        </section>
+    </main>
 </template>
 
 <style>
--- src/helpers/dictionary.ts
+++ src/helpers/dictionary.ts
@@ -1,7 +1,7 @@
-import type { DictionaryDataInterface, RawDictionaryDataInterface } from "@/types/dictionary"
-import { RedBlackTree } from "@/structures/red-black-tree"
-import { barrelRoll, flip, spin } from "@/helpers/gimmick"
-import dictionaryData from "@/assets/dictionary-data.json"
+import type { DictionaryDataInterface, RawDictionaryDataInterface } from "@/types/dictionary";
+import { RedBlackTree } from "@/structures/red-black-tree";
+import { barrelRoll, flip, spin } from "@/helpers/gimmick";
+import dictionaryData from "@/assets/dictionary-data.json";
 
 const rawDictionaryData: RawDictionaryDataInterface[] = dictionaryData.data;
 
@@ -9,40 +9,37 @@ const EN_RBT: RedBlackTree = new RedBlackTree();
 const ID_RBT: RedBlackTree = new RedBlackTree();
 
 export const gimmickNodes: DictionaryDataInterface[] = [
-  {
-    "keyEng": "Barrel Roll",
-    "descriptionEng": "Do a roll",
-    "keyIdn": "Muter Muter",
-    "descriptionIdn": "Muter muter",
-    gimmick: barrelRoll,
-  },
-  {
-    "keyEng": "Flip",
-    "descriptionEng": "Do a flip",
-    "keyIdn": "Muter Muter",
-    "descriptionIdn": "Muter muter",
-    gimmick: flip,
-  },
-  {
-    "keyEng": "Spin",
-    "descriptionEng": "Do a spin",
-    "keyIdn": "Muter Muter",
-    "descriptionIdn": "Muter muter",
-    gimmick: spin,
-  },
+    {
+        keyEng: "Barrel Roll",
+        descriptionEng: "Do a roll",
+        keyIdn: "Muter Muter",
+        descriptionIdn: "Muter muter gawe mumet, muter muter gawe mumet, muter ning ngawon, muter ning ngido, muter muter-muter gawe mumet...",
+        gimmick: barrelRoll,
+    },
+    {
+        keyEng: "Flip",
+        descriptionEng: "Do a flip",
+        keyIdn: "Muter Muter",
+        descriptionIdn: "Muter muter",
+        gimmick: flip,
+    },
+    {
+        keyEng: "Spin",
+        descriptionEng: "Do a spin",
+        keyIdn: "Muter Muter",
+        descriptionIdn: "Muter muter",
+        gimmick: spin,
+    },
 ];
 
 rawDictionaryData.forEach((rawDictionaryData: RawDictionaryDataInterface): void => {
-  EN_RBT.add(rawDictionaryData.keyEng, rawDictionaryData.descriptionEng, rawDictionaryData.keyIdn, rawDictionaryData.descriptionIdn);
-  ID_RBT.add(rawDictionaryData.keyIdn, rawDictionaryData.descriptionIdn, rawDictionaryData.keyEng, rawDictionaryData.descriptionEng);
+    EN_RBT.add(rawDictionaryData.keyEng, rawDictionaryData.descriptionEng, rawDictionaryData.keyIdn, rawDictionaryData.descriptionIdn);
+    ID_RBT.add(rawDictionaryData.keyIdn, rawDictionaryData.descriptionIdn, rawDictionaryData.keyEng, rawDictionaryData.descriptionEng);
 });
 
 gimmickNodes.forEach((node: DictionaryDataInterface): void => {
-  EN_RBT.add(node.keyEng, node.descriptionEng, node.keyIdn, node.descriptionIdn, node.gimmick);
-  ID_RBT.add(node.keyIdn, node.descriptionIdn, node.keyEng, node.descriptionEng, node.gimmick);
-})
+    EN_RBT.add(node.keyEng, node.descriptionEng, node.keyIdn, node.descriptionIdn, node.gimmick);
+    ID_RBT.add(node.keyIdn, node.descriptionIdn, node.keyEng, node.descriptionEng, node.gimmick);
+});
 
-export {
-  EN_RBT,
-  ID_RBT,
-}
+export { EN_RBT, ID_RBT };
